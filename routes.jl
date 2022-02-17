de2ed50022ad71fdb5e675e7772d7948106dfe26
Jakob Philippe <philippe.j@northeastern.edu>
2022-02-16 21:33:36 -0500

    update proj toml

--- routes.jl
+++ routes.jl
@@ -1,13 +1,15 @@
+include("src/MarinoApi.jl")
 using Genie.Router, Genie.Renderer.Json
+using MarinoApi
 
 route("/") do
   serve_static_file("welcome.html")
 end
 
-# route("/get_latest_data") do
-#   try
-#     json(get_latest_data())
-#   catch e
-#     @error e
-#   end
-# end
+route("/get_latest_data") do
+  try
+    json(get_latest_data())
+  catch e
+    @error e
+  end
+end

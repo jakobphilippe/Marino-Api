f85a41f67dda964c7a77e87ad16279d23a619ac5
Jakob Philippe <philippe.j@northeastern.edu>
2022-02-16 21:09:30 -0500

    added api code

--- routes.jl
+++ routes.jl
@@ -1,5 +1,15 @@
-using Genie.Router
+using Genie.Router, Genie.Renderer.Json
+using Mongoc
+using MarinoApi
 
 route("/") do
   serve_static_file("welcome.html")
+end
+
+route("/get_latest_data") do
+  try
+    json(get_latest_data())
+  catch e
+    @error e
+  end
 end
--- src/MarinoApi.jl
+++ src/MarinoApi.jl
@@ -1,6 +1,13 @@
 module MarinoApi
 
 using Genie, Logging, LoggingExtras
+using Logging
+using LoggingExtras
+using Mongoc
+using PrettyPrint
+using Genie, Logging, LoggingExtras
+
+locations = ["m-wr", "m-3", "m-g", "m-2", "m-tr", "sb-4"]
 
 function main()
   Core.eval(Main, :(const UserApp = $(@__MODULE__)))
@@ -9,6 +16,42 @@ function main()
 
   Core.eval(Main, :(const Genie = UserApp.Genie))
   Core.eval(Main, :(using Genie))
+end
+
+function get_latest_data()
+  suffix = ""
+  password = ""
+
+  if !isnothing(ENV["MONGO_PASSWORD"])
+    password = ENV["MONGO_PASSWORD"]
+  end
+
+  if !isnothing(ENV["CERT_LOC"])
+    suffix = ENV["CERT_LOC"]
+  end
+
+
+  client = Mongoc.Client("mongodb+srv://root:$password@marinobase.vunm9.mongodb.net/umongo?retryWrites=true&w=majority" * suffix)
+   
+  database = client["Umongo"]
+  @info "Succesfully connected to Umongo for API ;)"
+  collection = database["GymData"]
+  
+  marino_data_dict = Dict()
+  
+  for name in locations
+    bson_filter = Mongoc.BSON("""{ "name" : "$name" }""")
+    bson_options = Mongoc.BSON("""{ "sort" : { "date_time" : -1 } }""")
+    doc = Mongoc.find_one(collection, bson_filter, options=bson_options)
+    
+    marino_data_dict[name] = Dict(
+                              "number" => doc["number"],
+                              "percent" => doc["percent_full"],
+                              "date_time" => doc["date_time"]
+                              )
+  end
+
+  return marino_data_dict
 end
 
 end
